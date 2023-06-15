3fa62a1c85a6702ee8d8d2217fd51db2b80c243c
Milla2000 <millajesso2000@gmail.com>
2023-06-15 13:19:25 +0300

    debugging

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,7 @@
-#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
-
+#include "lists.h
 /**
  * print_dlistint - print all the elements of a dlistint_t list
  * @h: pointer to the start of the linked list
